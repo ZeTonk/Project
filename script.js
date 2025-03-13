8ded9cf5e68f9292e37a045d88f87e4b2b488d2e
ZeTonk <demian.english@outlook.com>
2025-03-13 13:27:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function reveal(){
+    document.getElementById('appear').style.display='block' ;
+    document.getElementById('dissapear').style.display='none';
+}
+function next(){
+    document.getElementById('Q1').style.display='block' ;
+    document.getElementById('appear').style.display='none';
+}
+function next1(){
+    document.getElementById('Q2').style.display='block' ;
+    document.getElementById('Q1').style.display='none';
+}
+function next2(){
+    document.getElementById('Q3').style.display='block'; 
+    document.getElementById('Q2').style.display='none';
+}
+function next3(){
+    document.getElementById('Q3').style.display='block'; 
+    document.getElementById('Q4').style.display='none';
+}
+
