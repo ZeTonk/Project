222e49af86dfffd16b3cc244911b0928fed374c6
ZeTonk <demian.english@outlook.com>
2025-04-09 14:14:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -15,12 +15,54 @@ function next2(){
     document.getElementById('Q2').style.display='none';
 }
 function next3(){
-    document.getElementById('Q3').style.display='block'; 
+    document.getElementById('Q4').style.display='block'; 
+    document.getElementById('Q3').style.display='none';
+}
+function next4(){
+    document.getElementById('Q5').style.display='block'; 
     document.getElementById('Q4').style.display='none';
 }
+function next5(){
+    document.getElementById('Q6').style.display='block'; 
+    document.getElementById('Q5').style.display='none';
+}
+function next6(){
+    document.getElementById('Finish').style.display='block'; 
+    document.getElementById('Q6').style.display='none';
+}
+
 
 function hide() {
     document.getElementById('hide').style.display= 'none';
-    document.getElementById('wrong').style.display= 'none'
-    
+    document.getElementById('wrong').style.display= 'none';
+}
+
+function hide1() {
+    document.getElementById('hide1').style.display= 'none';
+    document.getElementById('wrong1').style.display= 'none';
+}
+
+function hide2() {
+    document.getElementById('hide2').style.display= 'none';
+    document.getElementById('wrong2').style.display= 'none';
+}
+
+function hide3() {
+    document.getElementById('hide3').style.display= 'none';
+    document.getElementById('wrong3').style.display= 'none';
+}
+
+function hide4() {
+    document.getElementById('hide4').style.display= 'none';
+    document.getElementById('wrong4').style.display= 'none';
+}
+
+function hide5() {
+    document.getElementById('hide5').style.display= 'none';
+    document.getElementById('wrong5').style.display= 'none';
+}
+
+function hide6() {
+    document.getElementById('hide6').style.display= 'none';
+    document.getElementById('wrong6').style.display= 'none';
 }
